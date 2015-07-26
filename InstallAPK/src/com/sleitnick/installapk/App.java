237bc165f9ea03702b1cd40c0f23a0e00f67dc25
Stephen Leitnick <sleitnick@gmail.com>
2015-07-26 10:34:43 -0400

    First commit

---
+++ InstallAPK/src/com/sleitnick/installapk/App.java
@@ -0,0 +1,13 @@
+package com.sleitnick.installapk;
+
+public class App {
+	
+	public App() {
+		System.out.println("Hello from app!");
+	}
+	
+	public static void main(String[] args) {
+		new App();
+	}
+
+}
