18f7ebee61f831143ad6212dfd7729a21b9d7611
Stephen Leitnick <sleitnick@gmail.com>
2015-07-26 13:15:32 -0400

    Code fix

--- InstallAPK/src/com/sleitnick/installapk/App.java
+++ InstallAPK/src/com/sleitnick/installapk/App.java
@@ -11,7 +11,6 @@ import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 
 import javax.imageio.ImageIO;
